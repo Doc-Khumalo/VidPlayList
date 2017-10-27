0a6eaf2b981abd38690bb7a99c25a2a44665418b
Leslie <leslie.khumalo@icloud.com>
2017-10-27 01:04:17 +0100

    initial commit
    video playlist build

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import VideoPlayList from './app/VideoPlayList/VideoPlayList';
+import ReactDOM from 'react-dom';
+
+const app = document.querySelector('.js-app');
+
+ReactDOM.render(
+  <VideoPlayList />,
+app);
---
+++ src/app/VideoMain/VideoMain.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../VideoMain/VideoMain.css';
+import text from "../../../text";
+
+class VideoMain extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 'allVideos'
+        }
+    }
+    
+    backClick() {
+        this.props.allVideos(this.state.page)
+    }
+
+    render() {
+        const { video } = this.props;
+        const videoId = video.snippet.resourceId.videoId;
+
+        return (
+
+            <div className="container-fluid">
+                <div className="header-wrapper">
+                <a href="#" onClick={e => this.backClick()}>back to all videos</a>
+                    <h3>{video.snippet.title}</h3>
+                    <h6>{text.publishedHeader} {video.snippet.publishedAt}</h6>
+                </div>
+                <div className="row block-wrapper">
+                    <div className="col-md-6 col-lg-6">
+                        <iframe className="video-wrapper" width="420" height="390"
+                            src={`https://www.youtube.com/embed/${videoId}`}
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div className="col-md-6 col-lg-6">
+                        <p className="video-decsription">{video.snippet.description}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default VideoMain;
